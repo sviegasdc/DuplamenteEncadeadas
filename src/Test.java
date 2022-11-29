0e66dd4425ad96acc4e00496315c788b5e7f1ee0
S <77491046+sviegasdc@users.noreply.github.com>
2022-11-29 15:54:12 -0300

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,12 @@
 public class Test {
     public static void main(String[] args) {
         TADSequencia Lista = new TADSequencia();
-        Lista.insertFirst("batata");
-        Lista.insertLast("xavier");
+        No n = Lista.insertFirst("batata");
+        No p = Lista.insertFirst("bata");
+        No q = Lista.insertLast("number");
+        No u = Lista.insertLast("wed");
+        Lista.first();
+        System.out.println(Lista.elemAtRank(q));
         System.out.println(Lista);
         System.out.println("Lista vazia: "+Lista.isEmpty());
     }
